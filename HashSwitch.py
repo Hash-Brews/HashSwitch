bb900a78440224a08f1e35d15e1ae42b31e9e65e
PuppyStudios1 <148756635+PuppyStudios1@users.noreply.github.com>
2023-11-05 21:18:46 +1100

    Add files via upload

--- HashSwitch.py
+++ HashSwitch.py
@@ -1,44 +1,71 @@
-import pysimplegui as sg
+import os
+import subprocess
+import tkinter as tk
+from PIL import ImageTk
 
-# Define the hashSwitch entry
-hashSwitch = hbmenu.HBMenuEntry(
-    name="hashSwitch",
-    description="A homebrew app store for the Nintendo Switch",
-    icon="icon.png",
-    launch=launch_hashSwitch,
-    id="hashSwitch"
-)
+class HashSwitchCustomFirmware:
+    def __init__(self):
+        # Load the icon.png file
+        icon_image = ImageTk.PhotoImage(file="icon.png")
 
-# Launch hashSwitch
-def launch_hashSwitch():
-  # TODO: Implement the hashSwitch logic here
-  print("hashSwitch launched!")
+        # Start the HashSwitch GUI
+        self.root = tk.Tk()
+        self.root.title("HashSwitch GUI")
+        self.root.iconphoto(True, icon_image)
 
-# Create a window
-window = sg.Window("hashSwitch", layout=[[sg.Button("Launch hashSwitch")]])
+        # ...
 
-# Event loop
-while True:
-  event, values = window.read()
+    def main(self):
+        # Bypass the security checks that prevent unauthorized software from running
+        os.system("sysctl -w kernel.security.secureboot=0")
 
-  if event == sg.WIN_CLOSED:
-    break
+        # Start the mainloop
+        self.root.mainloop()
 
-  elif event == "Launch hashSwitch":
-    launch_hashSwitch()
+class HashSwitchGUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("HashSwitch GUI")
 
-# Close the window
-window.close()
+        # Create a frame for the Homebrew app loader
+        self.homebrew_frame = tk.LabelFrame(master=self.window, text="Homebrew")
+        self.homebrew_frame.grid(row=0, column=0, columnspan=2)
 
-# Add the hashSwitch entry to the hbmenu
-hbmenu.add_entry(hashSwitch)
+        # Add a button to browse for and select a Homebrew app file
+        self.homebrew_browse_button = tk.Button(master=self.homebrew_frame, text="Browse", command=self.homebrew_browse_clicked)
+        self.homebrew_browse_button.grid(row=0, column=0)
 
-# Start the hbmenu
-hbmenu.start()
+        # Add a button to run the selected Homebrew app
+        self.homebrew_run_button = tk.Button(master=self.homebrew_frame, text="Run", command=self.homebrew_run_clicked)
+        self.homebrew_run_button.grid(row=0, column=1)
 
-# Wait for the user to press the A button before returning to the Atmosphere bootloader
-while hbmenu.poll_input() != hbmenu.HBMENU_BUTTON_PRESS_A:
-  pass
+        # Create a frame for the Nintendo Switch firmware manager
+        self.firmware_frame = tk.LabelFrame(master=self.window, text="Firmware")
+        self.firmware_frame.grid(row=1, column=0, columnspan=2)
 
-# Return to the Atmosphere bootloader
-hbmenu.return_to_bootloader()
+        # Add a button to update the firmware
+        self.firmware_update_button = tk.Button(master=self.firmware_frame, text="Update", command=self.firmware_update_clicked)
+        self.firmware_update_button.grid(row=0, column=0)
+
+        # Add a button to downgrade the firmware
+        self.firmware_downgrade_button = tk.Button(master=self.firmware_frame, text="Downgrade", command=self.firmware_downgrade_clicked)
+        self.firmware_downgrade_button.grid(row=0, column=1)
+
+        # Add a button to install custom firmware
+        self.firmware_custom_button = tk.Button(master=self.firmware_frame, text="Custom", command=self.firmware_custom_clicked)
+        self.firmware_custom_button.grid(row=0, column=2)
+
+        # Start the mainloop
+        self.window.mainloop()
+
+    def homebrew_browse_clicked(self):
+        # Browse for and select a Homebrew app file
+        homebrew_file = tk.filedialog.askopenfilename(title="Select a Homebrew app file", filetypes=[("Homebrew app files", "*.nro")])
+
+        # If a Homebrew app file was selected, store the file path
+        if homebrew_file:
+            self.homebrew_file = homebrew_file
+
+    def homebrew_run_clicked(self):
+        # If a Homebrew app file was selected, run it
+        if self.homebrew_file
